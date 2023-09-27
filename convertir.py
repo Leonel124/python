7fda20fb5979b2010e73d1db00e5a34d8eb43b81
Leonel Sidharta <leonelsidhata922@gmail.com>
2023-09-27 14:28:10 -0600

    Se mejoró el codigo ya que el anterior contaba con codigo rendundante

--- convertir.py
+++ convertir.py
@@ -9,23 +9,27 @@ def angulos_radianes(grados):
     return radianes
 
 control = True
-
 sino = [1, 2]
-while control == True:
-    try:
-        pregunta = int(input("Escribe 1 si quieres transformar grados a radianes\nEscribe 2 si quieres transformar radianes a grados.\nIntroduzca un numero segun lo que quiers hacer: "))
-        valor = float(input("Introduzca el valor que quiere transformar: "))
-    except:
-        print("Entrada no válida. Por favor, introduzca un número válido.")
-        continue
 
-    if pregunta not in sino:
-        print("Introduzca una opción válida (1 o 2)")
-        control == False
-    else:
-        if pregunta == 1:
-            resultado = angulos_radianes(valor)
-            print(valor, "grados son equivalentes a", resultado, "radianes")
+while control:
+    try:
+        pregunta = int(input(
+            "Escribe 1 si quieres transformar grados a radianes\nEscribe 2 si quieres transformar radianes a grados.\nIntroduzca el numero correspondiente segun lo que quieres hacer: "))
+        valor = float(input("Introduzca el valor que quieres transformar: "))
+
+        if valor <= 0:
+            print("Introduzca un valor válido")
         else:
-            resultado = radianes_angulos(valor)
-            print(valor, "radianes son equivalentes a", resultado, "grados")
+            if pregunta not in sino:
+                print("Introduzca una opción válida. ")
+            else:
+                if pregunta == 1:
+                    resultado = angulos_radianes(valor)
+                    print(valor, "grados son equivalentes a", resultado, "radianes")
+                    control = False
+                else:
+                    resultado = radianes_angulos(valor)
+                    print(valor, "radianes son equivalentes a", resultado, "grados")
+                    control = False
+    except ValueError:
+        print("Entrada no válida. Por favor, introduzca un número válido.")
